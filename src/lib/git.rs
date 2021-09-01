814b5d5cfeffe7689b691b8100807424bc656089
flasco <flasco@163.com>
2021-09-01 10:46:50 +0800

    fix: 修改标志位判断形式

--- src/lib/git.rs
+++ src/lib/git.rs
@@ -1,18 +1,16 @@
 use std::process::Command;
 
 pub fn checkout_branch(branch_name: &str) -> Result<String, String> {
-  let res = Command::new("git")
-    .arg("checkout")
-    .arg("-b")
-    .arg(branch_name)
-    .output()
-    .unwrap();
+    let res = Command::new("git")
+        .arg("checkout")
+        .arg("-b")
+        .arg(branch_name)
+        .output()
+        .unwrap();
 
-  println!("{:#?}", res);
-
-  if res.stderr.len() > 0 {
-    Err(String::from_utf8(res.stderr).unwrap())
-  } else {
-    Ok(String::from_utf8(res.stdout).unwrap())
-  }
+    if res.status.success() {
+        Ok(format!("Switched to a new branch '{}'", branch_name))
+    } else {
+        Err(String::from_utf8(res.stderr).unwrap())
+    }
 }
